d57d0bdfba772478e6108b48191186df58579905
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2022-03-20 15:54:59 +0500

    Initial Commit

---
+++ Fruitus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruitus
+//
+//  Created by Hamza Rafique Azad on 20/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruitus/FruitusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitusApp.swift
+//  Fruitus
+//
+//  Created by Hamza Rafique Azad on 20/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
